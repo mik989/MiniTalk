e71b98af06c9e1a645443a495bd4a4d4d1d35743
dani <danielecastellano94@gmail.com>
2023-01-03 18:39:03 +0100

    invio lentissimo

--- client.c
+++ client.c
@@ -2,10 +2,41 @@
 
 int main(int ac, char **av)
 {
-	if (ac == 2)
+	int i;
+	int k;
+	char c;
+
+	i = 0;
+	if (ac == 3)
 	{
 		int pid;
 		pid = ft_atoi(av[1]);
-		kill(pid, SIGUSR1);
+		while (av[2][i])
+		{
+			k = 8;
+			while(k--)
+			{
+				c = (av[2][i] >> k) & 1;
+				if(c)
+				{
+					kill(pid, SIGUSR1);
+					sleep(1);
+				}
+				else
+				{
+					kill(pid, SIGUSR2);
+					sleep(1);
+				}
+			}
+			i++;
+		}
+		
+		//kill(pid, SIGUSR1);
+		//kill(pid, SIGUSR1);
+		//kill(pid, SIGUSR1);
+	}
+	else
+	{
+		write(2, "Error! Invalid input\n", 22);
 	}
 }
--- server.c
+++ server.c
@@ -1,19 +1,37 @@
 #include "minitalk.h"
 
-void handler()
+void handler(int signal, siginfo_t *info, void *cazzo)
 {
-	ft_printf("\U0001F60A\n");
-	ft_printf("\U0000267B\n");
+	(void) info;
+	(void) cazzo;
+	if (signal == SIGUSR1)
+	{
+		write(1, "1", 1);
+	}
+	else if (signal == SIGUSR2)
+	{
+		write(1, "0", 1);
+	}
+
 }
 int main()
 {
 	struct sigaction	sa_signal;
+	sigset_t			mask;
 	int pid;
-	pid = getpid();
+
+	sigemptyset(&mask);
+	sigaddset(&mask, SIGINT);
+	sigaddset(&mask, SIGQUIT);
+	sa_signal.sa_flags = SA_SIGINFO;
+	sa_signal.sa_mask = mask;
+	sa_signal.sa_handler = 0;
 	sa_signal.sa_sigaction = handler;
+	pid = getpid();
 	ft_printf("PID is : %d\n", pid);
 	sigaction(SIGUSR1, &sa_signal, NULL);
-	//while (1)
+	sigaction(SIGUSR2, &sa_signal, NULL);
+	while (1)
 		pause();
 	
 }
